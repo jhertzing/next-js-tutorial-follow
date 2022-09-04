bab262d337fe36e68605dcb09969f29f70b3b1e2
jhertzing <47403677+jhertzing@users.noreply.github.com>
2022-09-04 11:09:01 -0500

    2 files modified, 2 added

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,21 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
-      <Link href="/"> Back to main</Link>
-    </>
+      <h2>
+        <Link href="/">
+          <a>Back to main</a>
+        </Link>
+      </h2>
+    </Layout>
   );
 }
